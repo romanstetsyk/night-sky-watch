a292d1b57c115592c9db5105d5aefe7512065e5c
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2023-02-08 14:15:10 +0000

    refactor onload event

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,16 @@
-// Plug in current date to date selector
-window.onload = document.querySelector("#dateInput").value = new Date()
-  .toISOString()
-  .split("T")[0];
-// Load data for today on page load
-window.onload = getCAdata();
+window.addEventListener(
+  "load",
+  async () => {
+    // Plug in current date to date selector
+    document.querySelector("#dateInput").value = new Date()
+      .toISOString()
+      .split("T")[0];
+    document.querySelector("#searchDate").addEventListener("click", getCAdata);
+    // Load data for today on page load
+    await getCAdata();
+  },
+  { once: true }
+);
 
 function addSpinner() {
   const backdrop = document.createElement("div");
@@ -37,15 +44,10 @@ async function getCAdata() {
   removeSpinner();
   document
     .querySelectorAll("[data-object]")
-    .forEach(e => e.addEventListener("click", openDetails));
-  document.querySelector("#searchDate").addEventListener("click", getCAdata);
+    .forEach((e) => e.addEventListener("click", openDetails));
 }
 
 async function openDetails(event) {
-  document
-    .querySelectorAll(".row-active")
-    .forEach(e => e.classList.remove("row-active"));
-
   let objName = event.currentTarget.getAttribute("data-object");
 
   const objectDetails = document.querySelector(
